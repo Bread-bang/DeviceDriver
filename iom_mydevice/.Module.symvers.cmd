e46596b4405b39520b1d878a05a84af61923050e
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2023-12-14 01:19:01 +0900

    Delete iom_mydevice/.Module.symvers.cmd

--- iom_mydevice/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/work/dd/iom_mydevice/Module.symvers :=  sed 's/ko$$/o/'  /root/work/dd/iom_mydevice/modules.order | scripts/mod/modpost -m -a    -o /root/work/dd/iom_mydevice/Module.symvers -e -i Module.symvers -T - 
